0262bc656e4cf340d18b852b4431efeef57b9777
AAA48574 <aaronp261@gmail.com>
2017-07-05 12:55:28 +0100

    Added optional way to process the audio as discrete or continuous

--- serial-electret-data-listener.py
+++ serial-electret-data-listener.py
@@ -8,12 +8,15 @@ port = '/dev/ttyACM0'
 baud = 9600
 serial_port = serial.Serial(port, baud)
 
+osc_string_target = "/eos/chan/17"
+
+
 class UDPClient:
 
     def __init__(self):
         # self.HOST, self.PORT = "127.0.0.1", 9999
         # self.HOST, self.PORT = "192.168.0.42", 9999
-        self.HOST, self.PORT = "10.100.164.28", 9999
+        self.HOST, self.PORT = "10.100.164.29", 9999
 
         # APEP SOCK_DGRAM is the socket type to use for UDP sockets
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -42,20 +45,39 @@ class UDPClient:
 
 udp_client = UDPClient()
 
+min_allowed = 0
+max_allowed = 100
 
-def handle_data(data):
+mic_range_min = 40
+mic_range_max = 1000
+
+
+def rescale(i_value):
+    return ((max_allowed - min_allowed) * (i_value * mic_range_min) / (mic_range_max - mic_range_min)) + min_allowed;
+
+
+def handle_data_for_continuous_control(data):
+    try:
+        electret_peak_sample = int(data)
+        rescaleed_peak_sample = rescale(electret_peak_sample)
+        udp_client.send_numerical_data(osc_string_target, rescaleed_peak_sample)
+    except Exception as e:
+        print("Error: " + str(e))
+
+
+def handle_data_for_discrete_output(data):
     try:
         print(data)
         electret_peak_sample = int(data)
         if electret_peak_sample < 50:
             print("very quiet")
-            udp_client.send_numerical_data("/etc/chan/17", 15)
+            udp_client.send_numerical_data(osc_string_target, 15)
         elif electret_peak_sample < 500:
             print("medium noise")
-            udp_client.send_numerical_data("/etc/chan/17", 50)
+            udp_client.send_numerical_data(osc_string_target, 50)
         else:
             print("noisy")
-            udp_client.send_numerical_data("/etc/chan/17", 100)
+            udp_client.send_numerical_data(osc_string_target, 100)
     except Exception as e:
         print("Error: " + str(e))
 
@@ -65,7 +87,7 @@ def read_from_port(ser):
     while True:
         reading = ser.readline()
         electret_peak_sample = reading
-        handle_data(electret_peak_sample)
+        handle_data_for_continuous_control(electret_peak_sample)
 
 
 if __name__ == "__main__":
